64db7d741d403c0aa8a602c280392318fa9a0e13
Nash Allan-Rahill <nashallanrahill@gmail.com>
2022-08-27 18:37:03 -0400

    Adding files

---
+++ README.md
@@ -0,0 +1,4 @@
+# Canvas Today
+A simple Chrome extension to automatically scroll to and highlight current session in "Course Schedule" homepages for courses hosted at: canvas.dartmouth.edu
+
+## Installation
---
+++ content.js
@@ -0,0 +1,52 @@
+// HELPERS
+// TODO: only look in certain container/element?
+function textInElement(element, text) {
+  found = element.textContent.includes(text)
+  return found;
+}
+
+Date.prototype.addDays = function(days) {
+  var date = new Date(this.valueOf());
+  date.setDate(date.getDate() + days);
+  return date;
+}
+
+function formatDate(date) {
+  const month = date.toLocaleString('default', { month: 'short' });
+  var datestr = month + " " + date.getDate() + ", " + date.getFullYear()
+  return datestr
+}
+
+
+// TODO: before executing need to make sure this is a canvas link & contains dates
+const url = new URL('', window.location.href);
+// console.log(url.pathname);
+// TODO: change this to work for any canvas page?
+if (url.hostname == "canvas.dartmouth.edu" && !!document.getElementById("schedule")) {
+  schedule = document.getElementById("schedule");
+
+  // 1. Get current datetime
+  dateCheck = new Date();
+  datestr = formatDate(dateCheck)
+
+  // Look for nearest (or next) datetime in page content
+  // TODO: should maxDaysToAdd be changed?
+  daysAdded = 0
+  maxDaysToAdd = 7
+  //console.log("Searching for: " + datestr)
+  dateFound = textInElement(schedule, datestr);
+  while (!dateFound && daysAdded <= maxDaysToAdd) {
+      dateCheck = dateCheck.addDays(1)
+      datestr = formatDate(dateCheck)
+      daysAdded++
+      //console.log("Searching for: " + datestr)
+      dateFound = textInElement(schedule, datestr);
+  }
+
+  // Convert to jumping link & execute
+  if (dateFound) {
+      prevURL = window.location.href;
+      newURL = prevURL + "#content:~:text=" + encodeURIComponent(datestr);
+      window.location.replace(newURL);
+  }
+}
