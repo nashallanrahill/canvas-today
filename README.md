10e76b50ea653f76933ce5abb04c7b6cd3872bd8
Nathaniel Allan-Rahill <nash@Nathaniels-MacBook-Air.local>
2023-05-01 23:38:18 -0400

    Icons and updated README in prep for chrome webstore publish

--- README.md
+++ README.md
@@ -2,3 +2,35 @@
 A simple Chrome extension to automatically scroll to and highlight current session in "Course Schedule" homepages for courses hosted at: canvas.dartmouth.edu
 
 ## Installation
+
+
+
+## Privacy Policy
+
+We take user privacy very seriously. Canvas Today does not collect any user data or personal information.
+
+### Information Collection and Use
+
+Canvas Today does not collect or store any user data, including personal information such as name, email address, or location. We do not track user activity, search history, or browsing behavior.
+
+### Data Storage
+
+Canvas Today does not store any user data or personal information on our servers or any third-party servers. All data related to the functioning of the extension is stored locally on the user's device and is not accessible by us.
+
+### Third-party Services
+
+Canvas Today does not use any third-party services or APIs that may collect user data or personal information.
+
+### Security
+
+We take appropriate security measures to protect user data and personal information. As Canvas Today does not collect any user data, there is no risk of data breach or unauthorized access.
+
+### Updates
+
+We may update this privacy policy from time to time to reflect changes in our practices or the extension's functionality. Users will be notified of any changes by updating the "last updated" date at the bottom of the policy.
+
+### Contact Us
+
+If you have any questions or concerns about our privacy policy or the way we handle user data, please contact us at canvas.today.ext@gmail.com.
+
+*Last updated: 5/1/2023*
--- content.js
+++ content.js
@@ -16,35 +16,109 @@ function formatDate(date) {
   return datestr
 }
 
+function docReady(fn) {
+  // see if DOM is already available
+  if (document.readyState === "complete" || document.readyState === "interactive") {
+      // call on next available tick
+      setTimeout(fn, 1);
+  } else {
+      document.addEventListener("DOMContentLoaded", fn);
+  }
+}    
+
+function getTimestart(timerangestr){
+  var timestart = timerangestr.split("-")[0];
+  if (!timestart.includes(" ") & timestart.includes("M")) {
+    len = timestart.length
+    timestart = timestart.substring(0,len-2) + " " + timestart.substring(len-2,len)
+  }
+  return timestart
+}
+
+function iterateDate(table) {
+// Test code
+  // TODO: separate some of this into helper functions
+  currdate = new Date();
+  match = null;
+  $(table).find('tr').each(function(i){
+    if (i != 0) {
+      var date = $(this).find('td:first').text().trim();
+      var timerangestr = $(this).find('td:nth-child(2)').html()
+      if (timerangestr != null) {
+        timerangestr = timerangestr.split("<br>")[0]
+        timestart = getTimestart(timerangestr)
+        timend = timerangestr.split("-")[1];
+        datetime = new Date(date+" "+timend);
+        if (datetime instanceof Date && !isNaN(datetime) && currdate < datetime) {
+          console.log("Found date")
+          match = this;
+          return false; // Stop iterating
+        }
+      }
+    }
+  })
+  return match;
+}   
+
+
+
 // MAIN
 const url = new URL('', window.location.href);
 // console.log(url.pathname);
 // TODO: change this to work for any canvas page?
-if (url.hostname == "canvas.dartmouth.edu" && !!document.getElementById("schedule")) {
-  schedule = document.getElementById("schedule");
+if (!!document.getElementById("schedule")) {
+  console.log("Running canvas today")
+  var table = $('#schedule table');
+  matchingElement = iterateDate(table)
+  console.log(match)
 
-  // 1. Get current datetime
-  dateCheck = new Date();
-  datestr = formatDate(dateCheck)
 
-  // Look for nearest (or next) datetime in page content
-  // TODO: should maxDaysToAdd be changed?
-  daysAdded = 0
-  maxDaysToAdd = 7
-  //console.log("Searching for: " + datestr)
-  dateFound = textInElement(schedule, datestr);
-  while (!dateFound && daysAdded <= maxDaysToAdd) {
-      dateCheck = dateCheck.addDays(1)
-      datestr = formatDate(dateCheck)
-      daysAdded++
-      //console.log("Searching for: " + datestr)
-      dateFound = textInElement(schedule, datestr);
-  }
+  // // 1. Get current datetime
+  // dateCheck = new Date();
+  // datestr = formatDate(dateCheck)
 
-  // Convert to jumping link & execute
-  if (dateFound) {
-      prevURL = window.location.href;
-      newURL = prevURL + "#content:~:text=" + encodeURIComponent(datestr);
-      window.location.replace(newURL);
-  }
+  // // Look for nearest (or next) datetime in page content
+  // // TODO: should maxDaysToAdd be changed?
+  // daysAdded = 0
+  // maxDaysToAdd = 7
+  // console.log("Searching for: " + datestr)
+  // dateFound = textInElement(schedule, datestr);
+  // while (!dateFound && daysAdded <= maxDaysToAdd) {
+  //     dateCheck = dateCheck.addDays(1)
+  //     datestr = formatDate(dateCheck)
+  //     daysAdded++
+  //     //console.log("Searching for: " + datestr)
+  //     dateFound = textInElement(schedule, datestr);
+  // }
+
+
+  // // Convert to jumping link & execute
+  // if (dateFound) {
+  //     // prevURL = window.location.href;
+  //     // console.log(prevURL)
+  //     // newURL = prevURL + "#content:~:text=" + encodeURIComponent(datestr);
+  //     // window.location.replace(newURL);
+  //   //setTimeout(()=> {
+  //   //$(document).ready(function() {
+  //   docReady(function() {
+  //       // DOM is loaded and ready for manipulation here
+  //     console.log('DOM ready')
+  //     console.log("Scrolling")
+  //     var xpath = "//td[contains(text(), '" + datestr + "')]";
+  //     console.log(xpath)
+  //     var matchingElement = document.evaluate(xpath, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+  //     console.log(matchingElement)
+  //     // matchingElement.scrollIntoView({behavior: 'smooth', block: 'center'});
+  //     // matchingElement.focus({preventScroll: true});
+  //     //matchingElement.scrollIntoView()
+    $([document.documentElement, document.body]).animate({
+        scrollTop: $(matchingElement).offset().top
+    }, 500);
+
+      // matchingElement.parentElement.style['background'] = '#FFDD3C'
+      matchingElement.style['background'] = '#FFDD3C'
+    //});
+  //}, 2000)
+    // });
+  // }
 }
